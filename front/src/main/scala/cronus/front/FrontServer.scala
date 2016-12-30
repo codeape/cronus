925fea95c5d028dacc9b0bb53b9e0e2c2462d5c6
Oscar Norlander <oscar-no@codeape.org>
2016-12-30 22:25:00 +0100

    Added a JsonMappingException mapper
     * The server now gives a bad request status on JsonMappingException.

--- front/src/main/scala/cronus/front/FrontServer.scala
+++ front/src/main/scala/cronus/front/FrontServer.scala
@@ -1,12 +1,14 @@
 package cronus.front
 
-import com.twitter.finatra.http.filters.CommonFilters
+import com.twitter.finagle.http.Request
+import com.twitter.finatra.http.filters.{CommonFilters, ExceptionMappingFilter}
 import com.twitter.finatra.http.HttpServer
 import com.twitter.finatra.http.routing.HttpRouter
 import cronus.common.controllers.AssetController
 import cronus.common.filter.{AuthFilter, LaxedAuthFilter}
 import cronus.common.modules.{AkkaActorSystemModule, CronusConfigFlagsModule, WebJarAssetModule}
 import cronus.front.controllers.{MainJsController, TimeController, TimeSheetController, UserLoginController}
+import cronus.front.exception.JsonMappingExceptionMapper
 
 object FrontServerMain extends FrontServer
 
@@ -21,10 +23,12 @@ class FrontServer extends HttpServer {
   override def configureHttp(router: HttpRouter) {
     router
       .filter[CommonFilters]
+      .filter[ExceptionMappingFilter[Request]]
       .add[AssetController]
       .add[UserLoginController]
       .add[MainJsController]
       .add[LaxedAuthFilter, TimeController] // Filter needed to handle empty token.
       .add[AuthFilter, TimeSheetController]
+      .exceptionMapper[JsonMappingExceptionMapper]
   }
 }
---
+++ front/src/main/scala/cronus/front/exception/JsonMappingExceptionMapper.scala
@@ -0,0 +1,23 @@
+package cronus.front.exception
+
+import com.twitter.finagle.http.{Request, Response}
+import com.twitter.finatra.http.exceptions.ExceptionMapper
+import com.twitter.finatra.http.response.ResponseBuilder
+import com.twitter.finatra.json.internal.caseclass.jackson.JacksonUtils
+import com.twitter.inject.Logging
+import javax.inject.{Inject, Singleton}
+
+import com.fasterxml.jackson.databind.JsonMappingException
+
+@Singleton
+class JsonMappingExceptionMapper @Inject()(response: ResponseBuilder)
+  extends ExceptionMapper[JsonMappingException]
+    with Logging {
+
+  override def toResponse(request: Request, e: JsonMappingException): Response = {
+    warn(e)
+    response
+      .badRequest
+      .jsonError(JacksonUtils.errorMessage(e))
+  }
+}
